e8a191263b3934408089dc02ea86223bbe30ea21
MRendraAnanditya212 <mrendraananditya@gmail.com>
2025-05-05 18:59:50 +0700

    membuat dereference

---
+++ Pointer.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+
+    // reference 
+    int &d = b;
+    cout << "nilai " << d << endl;
+    cout << "reference " << &d << endl;
+
+    int f =5;
+    int g =f;
+}
